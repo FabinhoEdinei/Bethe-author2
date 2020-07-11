6fa9dd70874774764525ecaa3b7488829e16abac
FabinhoEdinei <59501299+FabinhoEdinei@users.noreply.github.com>
2020-07-11 12:11:35 -0300

    Initial commit

---
+++ src/pages/Register/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
+
+export default function Register(){
+return (
+<div className="reagister-container">
+<img src={logoImg} alt="logo"/>
+</div>
+);
+}
